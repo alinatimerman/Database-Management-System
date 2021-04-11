1504f8449f1ef3b7fe0c52ccf7a095f8b7fdf52f
Alina Timerman <79532163+alinatimerman@users.noreply.github.com>
2021-04-11 14:44:23 +0300

    Add files via upload

---
+++ Laboratory1/Laboratory1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Laboratory1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(436, 159);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(474, 80);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(454, 159);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(821, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(821, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(821, 342);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(642, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(642, 288);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(642, 314);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(642, 340);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(587, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "staff id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(584, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "first name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(584, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "last name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(584, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "store id";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Parent table Stores";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(483, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Child table Staff";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Laboratory1/Laboratory1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //connect
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cs = new SqlConnection("Data Source=ALINA-PC\\SQLEXPRESS; Initial Catalog = Store; Integrated Security = True");
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+
+            da.SelectCommand = new SqlCommand("SELECT * FROM Stores", cs);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string connectionString = "Data Source=ALINA-PC\\SQLEXPRESS; Initial Catalog = Store; Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter;
+            DataSet ds = new DataSet();
+
+            DataGridViewRow dataGridViewRow = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex];
+            string idSelectat = dataGridViewRow.Cells[0].Value.ToString();
+
+            using (connection)
+            {
+                    connection.Open();
+
+                    string queryString = "SELECT * FROM Staff WHERE store_id=@store_id";
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.AddWithValue("@store_id", idSelectat);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    DataTable staff = new DataTable();
+                    staff.Load(reader);
+                    dataGridView2.DataSource = staff;
+
+                    reader.Close();
+                    connection.Close();
+
+                    adapter = new SqlDataAdapter(queryString, connection);
+         
+               
+               // Console.ReadLine();
+            }
+        }
+
+        //add
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection cs = new SqlConnection("Data Source=ALINA-PC\\SQLEXPRESS; Initial Catalog = Store; Integrated Security = True");
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                da.InsertCommand = new SqlCommand("INSERT INTO Staff (first_name, last_name, store_id) VALUES(@f, @l, @si)", cs);
+               // da.InsertCommand.Parameters.Add("@s", SqlDbType.Int).Value = Int32.Parse(textBox1.Text);
+                da.InsertCommand.Parameters.Add("@f", SqlDbType.VarChar,30).Value =textBox2.Text;
+                da.InsertCommand.Parameters.Add("@l", SqlDbType.VarChar, 30).Value = textBox3.Text;
+                da.InsertCommand.Parameters.Add("@si", SqlDbType.Int).Value = Int32.Parse(textBox4.Text);
+                cs.Open();
+                da.InsertCommand.ExecuteNonQuery();
+               
+                cs.Close();
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cs.Close();
+            }
+        }
+
+        //update
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=ALINA-PC\\SQLEXPRESS; Initial Catalog = Store; Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter;
+            DataSet ds = new DataSet();
+
+            string queryString = "update Staff set first_name=@f, last_name=@l, store_id=@si where staff_id=@s";
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@s", textBox1.Text);
+                command.Parameters.AddWithValue("@f", textBox2.Text);
+                command.Parameters.AddWithValue("@l", textBox3.Text);
+                command.Parameters.AddWithValue("@si", textBox4.Text);
+                
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    reader.Close();
+                   
+
+                    connection.Close();
+
+                    adapter = new SqlDataAdapter(queryString, connection);
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    connection.Close();
+                }
+
+            }
+        }
+
+        //delete
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=ALINA-PC\\SQLEXPRESS; Initial Catalog = Store; Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter;
+            DataSet ds = new DataSet();
+
+            string queryString = "delete from Staff where staff_id = @s";
+            
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@s", textBox1.Text);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    reader.Close();
+
+                    adapter = new SqlDataAdapter(queryString, connection);
+                    
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    connection.Close();
+                }
+
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string connectionString = "Data Source=ALINA-PC\\SQLEXPRESS; Initial Catalog = Store; Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter;
+            DataSet ds = new DataSet();
+
+            DataGridViewRow dataGridViewRow = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex];
+            string idSelectat = dataGridViewRow.Cells[0].Value.ToString();
+
+            using (connection)
+            {
+        
+                    connection.Open();
+
+                    string queryString = "SELECT * FROM Staff WHERE store_id=@si";
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.AddWithValue("@si", idSelectat);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    DataTable staff = new DataTable();
+                    staff.Load(reader);
+                    dataGridView2.DataSource = staff;
+
+                    int index = e.RowIndex;
+                    DataGridViewRow selectedRow = dataGridView2.Rows[index];
+
+                    textBox1.Text = selectedRow.Cells[0].Value.ToString();
+                    textBox2.Text = selectedRow.Cells[1].Value.ToString();
+                    textBox3.Text = selectedRow.Cells[2].Value.ToString();
+                    textBox4.Text = selectedRow.Cells[3].Value.ToString();
+                   
+
+                    reader.Close();
+                    connection.Close();
+
+                    adapter = new SqlDataAdapter(queryString, connection);
+            }
+        }
+    }
+}
